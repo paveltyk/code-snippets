a9e50942d14085deeaa370db93d8a29b806f24ab
Alexandr Nikiforov <belmymail@gmail.com>
2010-10-08 15:16:16 +0300

    User should be able to login and logout
    http://www.pivotaltracker.com/story/show/5547622

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # Sample resource route with sub-resources:
   #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
+
   # Sample resource route with more complex sub-resources
   #   map.resources :products do |products|
   #     products.resources :comments
@@ -33,6 +33,10 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "welcome"
   map.resources :users
+
+  map.login 'login', :controller => 'user_sessions', :action => 'new'
+  map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'
+  map.resources :user_sessions
 
   # See how all your routes lay out with "rake routes"
 
