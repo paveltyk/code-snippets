0108edb4a5b46ebeadc2a36bb58bca28e5e3320c
Nikiforov Alexandr <belmymail@gmail.com>
2010-10-13 16:53:58 +0300

    User should be able to login via EMAIL
    http://www.pivotaltracker.com/story/show/5657126

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+
+  def self.find_by_username_or_email(login)
+    User.find_by_username(login) || User.find_by_email(login)
+  end
+
   acts_as_authentic
   has_many :snippets, :dependent => :destroy
 end
--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,2 +1,4 @@
 class UserSession < Authlogic::Session::Base
+   attr_accessor :email, :password, :login
+   find_by_login_method :find_by_username_or_email   
 end
