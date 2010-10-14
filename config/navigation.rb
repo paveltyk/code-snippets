d1b7a0820c9b02fc263311a62e8fa44faec3db4f
PavelT <paveltyk@gmail.com>
2010-10-14 10:26:07 +0300

    Review "There should be a menu" feature
    http://www.pivotaltracker.com/story/show/5678005

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,8 +1,9 @@
 SimpleNavigation::Configuration.run do |navigation|
+  navigation.selected_class = 'active'
   navigation.items do |primary|
-    primary.item :register, 'Register', register_path, :if => Proc.new { !current_user }
+    primary.item :snippets, 'My Snippets', user_snippets_path(:current), :if => Proc.new { current_user }
     primary.item :login, 'Log In', login_path, :if => Proc.new { !current_user }
+    primary.item :register, 'Register', register_path, :if => Proc.new { !current_user }
     primary.item :logout, 'Log Out', logout_path, :if => Proc.new { current_user }
-    primary.item :snippets, 'Snippets', user_snippets_path(:current)
   end
 end
