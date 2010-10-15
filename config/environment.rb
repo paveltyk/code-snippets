1e00d3ab6262e245afde5cf91dcae0683e1c1863
PavelT <paveltyk@gmail.com>
2010-10-15 17:56:34 +0300

    Use authlogic as gem
    http://www.pivotaltracker.com/story/show/5706011

--- config/environment.rb
+++ config/environment.rb
@@ -19,9 +19,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'haml'
-  config.gem "simple-navigation"
-  config.gem 'jrails'
+  config.gem 'haml', :version => '2.2.24'
+  config.gem 'simple-navigation', :version => '2.5.3'
+  config.gem 'jrails', :version => '0.6'
+  config.gem 'authlogic', :version => '2.1.6'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
