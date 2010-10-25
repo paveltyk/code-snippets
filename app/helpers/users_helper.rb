4c78442d09d0fecb7b9378b7854e4cd9ab60bb70
Nikiforov Alexandr <belmymail@gmail.com>
2010-10-25 15:33:36 +0300

    integrate with gravatar (without gem)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,14 @@
 module UsersHelper
-  include Avatar::View::ActionViewSupport
+
+  DEFAULT_OPTIONS = {
+          :size => 60,
+          }
+
+  def gravatar(user, options={})
+    require 'digest/md5'
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    options = DEFAULT_OPTIONS.merge(options)
+    image_tag "http://www.gravatar.com/avatar/#{hash}\"?s=#{h options[:size]}\""
+  end
 end
