013d3924595bbd6cdad268772b647f3f69b16bf9
Nikiforov Alexandr <belmymail@gmail.com>
2010-11-03 03:22:41 +0200

    Auto registration capabilities
    https://www.pivotaltracker.com/story/show/5973133

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,30 @@
 class User < ActiveRecord::Base
+  has_many :snippets, :dependent => :destroy
 
   AX = {:email => "http://axschema.org/contact/email",
         :first => "http://axschema.org/namePerson/first",
         :last => "http://axschema.org/namePerson/last"}
 
   acts_as_authentic do |c|
-    c.openid_required_fields = [AX[:email], AX[:first], AX[:last], :email, :nickname]
+    c.validate_login_field = false
+    c.validate_email_field = false
+    c.openid_required_fields = ["email", "nickname", AX[:email], AX[:first], AX[:last]]
   end
 
-  has_many :snippets, :dependent => :destroy
+  def before_save
+    if self.username.nil? || self.username.blank?
+      self.username = "user#{Time.now.to_i}"
+    end
+  end
 
   def self.find_by_username_or_email(login)
     find_by_username(login) || find_by_email(login)
   end
 
   private
-
   def map_openid_registration(reg)
-    self.email = (reg["email"] || reg["http://axschema.org/contact/email"]).to_s if self.email.blank?
+    self.email = (reg["email"] || reg[AX[:email]]).to_s if self.email.blank?
     self.username = (reg["nickname"] || [reg[AX[:first]], reg[AX[:last]]].flatten.join(' ')).to_s if self.username.blank?
   end
 end
+  
--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,3 +1,5 @@
 class UserSession < Authlogic::Session::Base
-   find_by_login_method :find_by_username_or_email   
+  auto_register
+  #logout_on_timeout true
+  find_by_login_method :find_by_username_or_email
 end
