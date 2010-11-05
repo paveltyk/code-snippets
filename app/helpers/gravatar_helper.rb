ea2740ef5c19fd96858547a19853fa036b7985bd
PavelT <paveltyk@gmail.com>
2010-11-05 20:19:36 +0200

    Integrate with gravatar (default icon changed)
    https://www.pivotaltracker.com/story/show/5762047

--- app/helpers/gravatar_helper.rb
+++ app/helpers/gravatar_helper.rb
@@ -4,7 +4,7 @@ module GravatarHelper
   def gravatar_for(user, options={})
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
-    options.reverse_merge! :size => 60
-    image_tag "http://www.gravatar.com/avatar/#{hash}?s=#{h options[:size]}", :alt => user.username, :title => user.username
+    options.reverse_merge! :size => 60, :default_image => 'monsterid'
+    image_tag "http://www.gravatar.com/avatar/#{hash}?s=#{h options[:size]}&d=#{options[:default_image]}", :alt => user.username, :title => user.username
   end
 end
