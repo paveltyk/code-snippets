5e5b464db4e5df187dc7ab5c76cfd9e707d8e4fb
PavelT <paveltyk@gmail.com>
2010-11-13 19:45:01 +0200

    Change Login field placeholder
    https://www.pivotaltracker.com/story/show/6210557

--- config/navigation.rb
+++ config/navigation.rb
@@ -4,7 +4,7 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :new_snippet, 'New Snippet', new_my_snippet_path, :if => Proc.new { current_user }
     primary.item :snippets, 'My Snippets', my_snippets_path, :if => Proc.new { current_user }
     primary.item :profile, 'Edit Profile', edit_profile_path, :if => Proc.new { current_user }
-    primary.item :login, 'Login', login_path, :if => Proc.new { !current_user }
+    primary.item :login, 'Login', login_path, :if => Proc.new { !current_user }, :title => 'Login or Register'
     primary.item :logout, 'Logout', logout_path, :if => Proc.new { current_user }
 
     primary.dom_class = 'top-navigation'
